7c4b9400280828c67414047cdec34fe233350b2d
dthirst <demian.keihsler@imp.ac.at>
2022-01-20 12:08:09 +0100

    update readme comment

--- freeze-mf.py
+++ freeze-mf.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 
-# freeze-mf - tool for making montages of cryoEM screening images
+# freeze-mf - a simple tool for making montages of cryoEM screening images
 # written 2022 by demian keihsler at the institute of molecular pathology, vienna
 # ********
 # usage:
-#   save the files you want to mount in the following pattern in one folder:
+#   - save the files you want to mount in the following pattern in one folder:
 #       <name>_map.<ext>,
 #       <name>_square.<ext>, 
 #       <name>_hole.<ext>, 
 #       <name>_i1.<ext>, 
 #       <name>_i2.<ext>
-#   start the script by writing 'python3 <path-to-script>/freeze.mf -p <path-of-files>'
-#   the montages will be save in the same folder with the pattern <name)>_montage.png
-#   ! if you have a map file but no square and smaller, the montage will contain only the map. same for square but no hole, hole but no i1/i2.
-#   ! if you have a square file but no map, there will be no montage. same for hole but no square, i1/i2 but no hole.
-#   thank your local technician
+#   - you can have as many different groups of files as you want, as long as files that belong together
+#     always start with the same name and have the same extension. the script will iterate through the 
+#     folder and make a montage for each group that has at least a file whose name ends with _map.
+#   - start the script by writing 'python3 <path-to-script>/freeze.mf -p <path-of-files>'
+#   - the montages will be save in the same folder with the naming pattern <name>_montage.png
+#   - please mind the following expected behaviour: 
+#     - if you have a map file but no square and smaller, the montage will contain only the map. same for
+#       square but no hole, hole but no i1/i2.
+#     - if you have a square and/or any other file but no map, there will be no montage.
+#   thank your local technician 🍻
 # ********
 
 import argparse
